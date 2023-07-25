e33fc6eec54b7c31fe60e6f1421da83b14390652
Oleksandr Kushnirenko <okushnirenko.work@gmail.com>
2023-07-25 22:30:38 +0300

    Add UAssetValidator_Properties base implementation

---
+++ Source/AssetValidation/Public/AssetValidators/AssetValidator_Properties.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "AssetValidator.h"
+
+#include "AssetValidator_Properties.generated.h"
+UCLASS()
+class ASSETVALIDATION_API UAssetValidator_Properties: public UAssetValidator
+{
+	GENERATED_BODY()
+public:
+
+	//~Begin EditorValidatorBase 
+	virtual bool CanValidateAsset_Implementation(UObject* InAsset) const override;
+	virtual EDataValidationResult ValidateLoadedAsset_Implementation(UObject* InAsset, TArray<FText>& ValidationErrors) override;
+	//~End EditorValidatorBase
+};
