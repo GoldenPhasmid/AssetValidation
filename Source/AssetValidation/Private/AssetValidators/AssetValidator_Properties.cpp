8c5b5e0efbeec1f20da391e1fe5001d5447d56ff
Oleksandr Kushnirenko <okushnirenko.work@gmail.com>
2023-07-26 00:34:59 +0300

    Asset Property Validator first implementation pass

--- Source/AssetValidation/Private/AssetValidators/AssetValidator_Properties.cpp
+++ Source/AssetValidation/Private/AssetValidators/AssetValidator_Properties.cpp
@@ -3,9 +3,19 @@
 #include "PropertyValidatorSubsystem.h"
 #include "PropertyValidators/PropertyValidatorBase.h"
 
+UAssetValidator_Properties::UAssetValidator_Properties()
+{
+	
+}
+
 bool UAssetValidator_Properties::CanValidateAsset_Implementation(UObject* InAsset) const
 {
-	return Super::CanValidateAsset_Implementation(InAsset) && InAsset && InAsset->IsA(UBlueprint::StaticClass());
+	if (!Super::CanValidateAsset_Implementation(InAsset))
+	{
+		return false;
+	};
+
+	return InAsset && InAsset->IsA(UBlueprint::StaticClass());
 }
 
 EDataValidationResult UAssetValidator_Properties::ValidateLoadedAsset_Implementation(UObject* InAsset, TArray<FText>& ValidationErrors)
@@ -14,23 +24,73 @@ EDataValidationResult UAssetValidator_Properties::ValidateLoadedAsset_Implementa
 	check(PropertyValidators);
 
 	FPropertyValidationResult PropertyValidation;
-	for (TFieldIterator<FProperty> It(InAsset->GetClass(), EFieldIterationFlags::Default); It; ++It)
+	
+	UClass* Class = CastChecked<UBlueprint>(InAsset)->GeneratedClass;
+	UObject* Object = Class->GetDefaultObject();
+	check(Class && Object);
+	
+	while (Class && CanValidateClass(Class))
 	{
-		FProperty* Property = *It;
-		PropertyValidation.Append(PropertyValidators->IsPropertyValid(InAsset, Property));
+		if (bSkipBlueprintGeneratedClasses && IsBlueprintGeneratedClass(Class))
+		{
+			Class = Class->GetSuperClass();
+			continue;
+		}
+		
+		for (TFieldIterator<FProperty> It(Class, EFieldIterationFlags::None); It; ++It)
+		{
+			FProperty* Property = *It;
+			// do not validate transient or deprecated properties
+			// only validate properties that we can actually edit in editor
+			if (!Property->HasAnyPropertyFlags(EPropertyFlags::CPF_Transient) && Property->HasAnyPropertyFlags(EPropertyFlags::CPF_Edit))
+			{
+				PropertyValidation.Append(PropertyValidators->IsPropertyValid(Object, Property));
+			}
+		}
+		
+		Class = Class->GetSuperClass();
 	}
-
+	
 	if (PropertyValidation.ValidationResult == EDataValidationResult::Invalid)
 	{
 		for (const FText& Text: PropertyValidation.ValidationErrors)
 		{
-			AssetFails(InAsset, Text, ValidationErrors);
+			AssetFails(Object, Text, ValidationErrors);
 		}
 	}
 	else
 	{
-		AssetPasses(InAsset);
+		AssetPasses(Object);
 	}
 	
 	return PropertyValidation.ValidationResult;
+}
+
+bool UAssetValidator_Properties::CanValidateClass(UClass* Class) const
+{
+	if (IsBlueprintGeneratedClass(Class))
+	{
+		return true;
+	}
+	
+	const UPackage* Package = Class->GetPackage();
+	const FString PackageName = Package->GetName();
+
+#if WITH_EDITOR
+	if (PackageName.StartsWith("/Script/AssetValidation"))
+	{
+		return true;
+	}
+#endif
+	
+	return PackagesToValidate.ContainsByPredicate([PackageName](const FString& ModulePath)
+	{
+		return PackageName.StartsWith(ModulePath);
+	});
+}
+
+bool UAssetValidator_Properties::IsBlueprintGeneratedClass(UClass* Class) const
+{
+	const FString PackageName = Class->GetPackage()->GetName();
+	return PackageName.StartsWith(TEXT("/Game/"));
 }
