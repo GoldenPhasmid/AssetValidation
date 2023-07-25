77718159bda45068ddffd1eb4fc19ad162b544d8
Oleksandr Kushnirenko <okushnirenko.work@gmail.com>
2023-07-25 22:30:04 +0300

    Add assets in Developers folder and DirectoriesToNeverCook to exclude from validation

--- Source/AssetValidation/Public/AssetValidators/AssetValidator.h
+++ Source/AssetValidation/Public/AssetValidators/AssetValidator.h
@@ -11,5 +11,7 @@ class ASSETVALIDATION_API UAssetValidator: public UEditorValidatorBase
 {
 	GENERATED_BODY()
 public:
+
+	virtual bool CanValidateAsset_Implementation(UObject* InAsset) const override;
 	
 };
