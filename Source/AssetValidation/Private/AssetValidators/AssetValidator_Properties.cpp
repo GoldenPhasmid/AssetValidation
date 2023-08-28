9ede38b5af298e49a2b1b68b04c4436697c7f3e5
Oleksandr Kushnirenko <okushnirenko.work@gmail.com>
2023-08-28 23:44:35 +0300

    Allow validate objects with Property Asset Validator

--- Source/AssetValidation/Private/AssetValidators/AssetValidator_Properties.cpp
+++ Source/AssetValidation/Private/AssetValidators/AssetValidator_Properties.cpp
@@ -3,27 +3,17 @@
 #include "PropertyValidatorSubsystem.h"
 #include "PropertyValidators/PropertyValidation.h"
 
-UAssetValidator_Properties::UAssetValidator_Properties()
-{
-	
-}
-
-bool UAssetValidator_Properties::CanValidateAsset_Implementation(UObject* InAsset) const
-{
-	if (!Super::CanValidateAsset_Implementation(InAsset))
-	{
-		return false;
-	};
-
-	return InAsset && InAsset->IsA(UBlueprint::StaticClass());
-}
-
 EDataValidationResult UAssetValidator_Properties::ValidateLoadedAsset_Implementation(UObject* InAsset, TArray<FText>& ValidationErrors)
 {
 	UPropertyValidatorSubsystem* PropertyValidators = GEditor->GetEditorSubsystem<UPropertyValidatorSubsystem>();
 	check(PropertyValidators);
+
+	UClass* Class = InAsset->GetClass();
+	if (UBlueprint* Blueprint = Cast<UBlueprint>(InAsset))
+	{
+		Class = Blueprint->GeneratedClass;
+	}
 	
-	UClass* Class = CastChecked<UBlueprint>(InAsset)->GeneratedClass;
 	UObject* Object = Class->GetDefaultObject();
 	check(Class && Object);
 	
--- Source/AssetValidation/Public/AssetValidators/AssetValidator_Properties.h
+++ Source/AssetValidation/Public/AssetValidators/AssetValidator_Properties.h
@@ -10,10 +10,7 @@ class ASSETVALIDATION_API UAssetValidator_Properties: public UAssetValidator
 	GENERATED_BODY()
 public:
 
-	UAssetValidator_Properties();
-
 	//~Begin EditorValidatorBase 
-	virtual bool CanValidateAsset_Implementation(UObject* InAsset) const override;
 	virtual EDataValidationResult ValidateLoadedAsset_Implementation(UObject* InAsset, TArray<FText>& ValidationErrors) override;
 	//~End EditorValidatorBase
 
