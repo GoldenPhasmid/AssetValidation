7a2a1182341327589b3d765734dfb78f317f3c41
phasmid <okushnirenko.work@gmail.com>
2024-05-14 18:04:00 +0300

    Move property external data structures back to PropertyValidationSettings

--- Source/AssetValidation/Private/PropertyValidationSettings.cpp
+++ Source/AssetValidation/Private/PropertyValidationSettings.cpp
@@ -1,7 +1,5 @@
 #include "PropertyValidationSettings.h"
 
-#include "Engine/UserDefinedStruct.h"
-
 void UPropertyValidationSettings::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
 {
 	Super::PostEditChangeProperty(PropertyChangedEvent);
@@ -20,69 +18,5 @@ void UPropertyValidationSettings::PostEditChangeProperty(FPropertyChangedEvent& 
 		{
 			PropertyData.Struct = StructDesc.StructClass;
 		}
-	}
-}
-
-const TArray<FPropertyExternalValidationData>* UPropertyValidationSettings::GetMutableValidationData(const UStruct* Struct) const
-{
-	const auto Settings = Get();
-	if (const UUserDefinedStruct* UserDefinedStruct = Cast<UUserDefinedStruct>(Struct))
-	{
-		FSoftObjectPath StructPath = TSoftObjectPtr{UserDefinedStruct}.ToSoftObjectPath();
-		if (const FStructExternalValidationData* Entry = Settings->UserDefinedStructs.Find(StructPath))
-		{
-			return &Entry->Properties;
-		}
-	}
-	else if (const UClass* Class = Cast<UClass>(Struct))
-	{
-		if (const FClassExternalValidationData* Entry = Settings->ExternalClasses.FindByKey(Class))
-		{
-			return &Entry->Properties;
-		}
-	}
-	else if (const UScriptStruct* ScriptStruct = Cast<UScriptStruct>(Struct))
-	{
-		if (const FStructExternalValidationData* Entry = Settings->ExternalStructs.FindByKey(ScriptStruct))
-		{
-			return &Entry->Properties;
-		}
-	}
-
-	return nullptr;
-}
-
-const TArray<FPropertyExternalValidationData>& UPropertyValidationSettings::GetExternalValidationData(const UStruct* Struct)
-{
-	const auto Settings = Get();
-	if (const TArray<FPropertyExternalValidationData>* StructData = Settings->GetMutableValidationData(Struct))
-	{
-		return *StructData;
-	}
-	
-	static TArray<FPropertyExternalValidationData> EmptyData;
-	return EmptyData;
-}
-
-FPropertyExternalValidationData UPropertyValidationSettings::GetPropertyExternalValidationData(UStruct* Struct, FProperty* Property)
-{
-	const TArray<FPropertyExternalValidationData>& Properties = GetExternalValidationData(Struct);
-	auto Pred = [Property](const FPropertyExternalValidationData& PropertyData)
-	{
-		return PropertyData.GetProperty() == Property;
-	};
-	
-	if (const FPropertyExternalValidationData* PropertyData = Properties.FindByPredicate(Pred))
-	{
-		return *PropertyData;
-	}
-	return FPropertyExternalValidationData{Struct, Property};
-}
-
-void UPropertyValidationSettings::UpdatePropertyExternalValidationData(const FPropertyExternalValidationData& InData)
-{
-	if (const TArray<FPropertyExternalValidationData>* StructData = Settings->GetMutableValidationData(Struct))
-	{
-		
 	}
 }
