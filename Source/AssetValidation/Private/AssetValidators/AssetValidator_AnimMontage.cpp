6923570401d7540a43c4c455743a35ce04b8b064
phasmid <okushnirenko.work@gmail.com>
2024-08-05 20:41:03 +0300

    AnimMontage asset validator

---
+++ Source/AssetValidation/Private/AssetValidators/AssetValidator_AnimMontage.cpp
@@ -0,0 +1,59 @@
+﻿#include "AssetValidators/AssetValidator_AnimMontage.h"
+
+#include "AssetValidationDefines.h"
+#include "AssetValidationStatics.h"
+#include "AssetValidators/AssetValidator_AnimSequence.h"
+#include "Misc/DataValidation.h"
+
+#define LOCTEXT_NAMESPACE "AssetValidation"
+
+bool UAssetValidator_AnimMontage::CanValidateAsset_Implementation(const FAssetData& InAssetData, UObject* InObject, FDataValidationContext& InContext) const
+{
+	return Super::CanValidateAsset_Implementation(InAssetData, InObject, InContext) && InObject && InObject->IsA<UAnimMontage>();
+}
+
+EDataValidationResult UAssetValidator_AnimMontage::ValidateLoadedAsset_Implementation(const FAssetData& InAssetData, UObject* InAsset, FDataValidationContext& Context)
+{
+	TRACE_CPUPROFILER_EVENT_SCOPE_ON_CHANNEL(UAssetValidator_AnimMontage, AssetValidationChannel);
+
+	UAnimMontage* Montage = CastChecked<UAnimMontage>(InAsset);
+
+	const USkeleton* Skeleton = Montage->GetSkeleton();
+	if (!IsValid(Skeleton))
+	{
+		Context.AddMessage(UE::AssetValidation::CreateTokenMessage(
+			EMessageSeverity::Error, InAssetData, LOCTEXT("NoSkeleton", "animation is missing skeleton.")
+		));
+		return EDataValidationResult::Invalid;
+	}
+
+	TArray<UAnimationAsset*> AnimationAssets;
+	Montage->GetAllAnimationSequencesReferred(AnimationAssets);
+
+	const FAssetData SkeletonAsset{Skeleton};
+	EDataValidationResult Result = EDataValidationResult::Valid;
+	for (const UAnimationAsset* AnimAsset: AnimationAssets)
+	{
+		if (AnimAsset->GetSkeleton() != Skeleton)
+		{
+			Context.AddMessage(UE::AssetValidation::CreateTokenMessage(
+				EMessageSeverity::Error, InAssetData, LOCTEXT("NoSkeleton", "skeleton mismatch for animation asset "), FAssetData{AnimAsset}
+			));
+			Result &= EDataValidationResult::Invalid;
+		}
+
+		if (const UAnimSequenceBase* AnimSequence = Cast<UAnimSequenceBase>(AnimAsset))
+		{
+			Result &= UAssetValidator_AnimSequence::ValidateCurves(*Skeleton, *AnimSequence, InAssetData, Context);
+		}
+	}
+
+	if (Result != EDataValidationResult::Invalid)
+	{
+		AssetPasses(Montage);
+	}
+
+	return Result;
+}
+
+#undef LOCTEXT_NAMESPACE
---
+++ Source/AssetValidation/Public/AssetValidators/AssetValidator_AnimMontage.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "AssetValidator.h"
+
+#include "AssetValidator_AnimMontage.generated.h"
+
+UCLASS()
+class ASSETVALIDATION_API UAssetValidator_AnimMontage: public UAssetValidator
+{
+	GENERATED_BODY()
+public:
+	//~Begin EditorValidatorBase interface
+	virtual bool CanValidateAsset_Implementation(const FAssetData& InAssetData, UObject* InObject, FDataValidationContext& InContext) const override;
+	virtual EDataValidationResult ValidateLoadedAsset_Implementation(const FAssetData& InAssetData, UObject* InAsset, FDataValidationContext& Context) override;
+	//~End EditorValidatorBase interface
+};
