93cd8e19cfb07d9b708872012dc0a7908ddc0aa6
phasmid <okushnirenko.work@gmail.com>
2024-08-06 23:15:47 +0300

    asset naming convention validator

---
+++ Source/AssetValidation/Private/AssetValidators/AssetValidator_AssetNamingConventions.cpp
@@ -0,0 +1,67 @@
+﻿#include "AssetValidators/AssetValidator_AssetNamingConventions.h"
+
+#include "AssetValidationDefines.h"
+#include "AssetValidationStatics.h"
+#include "Misc/DataValidation.h"
+#include "PropertyValidators/PropertyValidation.h"
+
+#define LOCTEXT_NAMESPACE "AssetValidation"
+
+UAssetValidator_AssetNamingConventions::UAssetValidator_AssetNamingConventions()
+{
+	bIsConfigDisabled = false; // enabled by default
+}
+
+bool UAssetValidator_AssetNamingConventions::CanValidateAsset_Implementation(const FAssetData& InAssetData, UObject* InObject, FDataValidationContext& InContext) const
+{
+	if (!Super::CanValidateAsset_Implementation(InAssetData, InObject, InContext))
+	{
+		return false;
+	}
+	
+	const FString PackageName = InAssetData.PackageName.ToString();
+	return UE::AssetValidation::IsBlueprintGeneratedPackage(PackageName);
+}
+
+EDataValidationResult UAssetValidator_AssetNamingConventions::ValidateLoadedAsset_Implementation(const FAssetData& InAssetData, UObject* InAsset, FDataValidationContext& InContext)
+{
+	return ValidateAsset_Implementation(InAssetData, InContext);
+}
+
+EDataValidationResult UAssetValidator_AssetNamingConventions::ValidateAsset_Implementation(const FAssetData& InAssetData, FDataValidationContext& InContext)
+{
+	static const FRegexPattern AllowedCharacters{TEXT("^[A-Za-z0-9_-]*$")};
+
+	EDataValidationResult Result = EDataValidationResult::Valid;
+
+	const FString PackageName = FPackageName::GetShortName(InAssetData.PackageName);
+	if (PackageName.IsEmpty())
+	{
+		InContext.AddMessage(UE::AssetValidation::CreateTokenMessage(
+			EMessageSeverity::Error, InAssetData,
+			LOCTEXT("AssetNamingConvention_InvalidPackage", "Failed to find a valid package name."))
+		);
+		Result &= EDataValidationResult::Invalid;
+	}
+	// fast check for first letter
+	else if (PackageName[0] < TCHAR{'A'} || PackageName[0] > TCHAR{'Z'})
+	{
+		InContext.AddMessage(UE::AssetValidation::CreateTokenMessage(
+			EMessageSeverity::Error, InAssetData,
+			LOCTEXT("AssetNamingConvention_InitialCharacter", "Initial character should be a capitalized A-Z letter."))
+		);
+		Result &= EDataValidationResult::Invalid;
+	}
+	else if (FRegexMatcher Matcher{AllowedCharacters, PackageName}; !Matcher.FindNext())
+	{
+		InContext.AddMessage(UE::AssetValidation::CreateTokenMessage(
+			EMessageSeverity::Error, InAssetData,
+			LOCTEXT("AssetNamingConvention_NonASCIICharacters", "Asset name contains non-ASCII characters or illegal symbols."))
+		);
+		Result &= EDataValidationResult::Invalid;
+	}
+	
+	return Result;
+}
+
+#undef LOCTEXT_NAMESPACE
---
+++ Source/AssetValidation/Public/AssetValidators/AssetValidator_AssetNamingConventions.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "AssetValidator.h"
+
+#include "AssetValidator_AssetNamingConventions.generated.h"
+
+UCLASS()
+class UAssetValidator_AssetNamingConventions: public UAssetValidator
+{
+	GENERATED_BODY()
+public:
+
+	UAssetValidator_AssetNamingConventions();
+	
+	//~Begin EditorValidatorBase interface
+	virtual bool CanValidateAsset_Implementation(const FAssetData& InAssetData, UObject* InObject, FDataValidationContext& InContext) const override;
+	virtual EDataValidationResult ValidateAsset_Implementation(const FAssetData& InAssetData, FDataValidationContext& InContext) override;
+	virtual EDataValidationResult ValidateLoadedAsset_Implementation(const FAssetData& InAssetData, UObject* InAsset, FDataValidationContext& InContext) override;
+	//~End EditorValidatorBase interface
+};
