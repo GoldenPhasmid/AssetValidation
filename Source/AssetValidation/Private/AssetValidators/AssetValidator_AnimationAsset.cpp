b89a96fd476fe5cbb71263c888e5f851e23d220a
Oleksandr <okushnirenko.work@gmail.com>
2024-08-25 12:01:41 +0300

    Create asset validator for animation curves for all animation assets

---
+++ Source/AssetValidation/Private/AssetValidators/AssetValidator_AnimationAsset.cpp
@@ -0,0 +1,130 @@
+#include "AssetValidators/AssetValidator_AnimationAsset.h"
+
+#include "AssetValidationDefines.h"
+#include "AssetValidationStatics.h"
+
+#define LOCTEXT_NAMESPACE "AssetValidation"
+
+bool UAssetValidator_AnimationAsset::CanValidateAsset_Implementation(const FAssetData& InAssetData, UObject* InObject, FDataValidationContext& InContext) const
+{
+	if (!Super::CanValidateAsset_Implementation(InAssetData, InObject, InContext))
+	{
+		return false;
+	}
+
+	static struct FDerivedClasses
+	{
+		FDerivedClasses(const UClass* BaseClass)
+		{
+			TArray<UClass*> Classes;
+			GetDerivedClasses(BaseClass, Classes);
+
+			for (const UClass* Class: Classes)
+			{
+				ClassPaths.Add(Class->GetClassPathName());
+			}
+		}
+		
+		const TArray<FTopLevelAssetPath>& Get()
+		{
+			return ClassPaths;
+		}
+
+		TArray<FTopLevelAssetPath> ClassPaths;
+	} Classes{UAnimationAsset::StaticClass()};
+
+	return InObject != nullptr && Classes.Get().Contains(InAssetData.AssetClassPath);
+}
+
+EDataValidationResult UAssetValidator_AnimationAsset::ValidateLoadedAsset_Implementation(const FAssetData& InAssetData, UObject* InAsset, FDataValidationContext& Context)
+{
+	TRACE_CPUPROFILER_EVENT_SCOPE_ON_CHANNEL(UAssetValidator_AnimationAsset, AssetValidationChannel);
+
+	UAnimationAsset* AnimationAsset = CastChecked<UAnimationAsset>(InAsset);
+	const USkeleton* BaseSkeleton = AnimationAsset->GetSkeleton();
+	
+	EDataValidationResult Result = EDataValidationResult::Valid;
+	
+	Result &= ValidateSkeleton(BaseSkeleton, *AnimationAsset, InAssetData, Context);
+	Result &= ValidateCurves(*AnimationAsset, InAssetData, Context);
+	
+	TArray<UAnimationAsset*> AnimationAssets;
+	AnimationAsset->GetAllAnimationSequencesReferred(AnimationAssets);
+	
+	for (const UAnimationAsset* AnimAsset: AnimationAssets)
+	{
+		Result &= ValidateSkeleton(BaseSkeleton, *AnimAsset, InAssetData, Context);
+		Result &= ValidateCurves(*AnimAsset, InAssetData, Context);
+	}
+
+	if (Result != EDataValidationResult::Invalid)
+	{
+		AssetPasses(AnimationAsset);
+	}
+
+	return Result;
+}
+
+EDataValidationResult UAssetValidator_AnimationAsset::ValidateSkeleton(const USkeleton* Skeleton, const UAnimationAsset& AnimAsset, const FAssetData& AssetData, FDataValidationContext& Context)
+{
+	const USkeleton* AnimSkeleton = AnimAsset.GetSkeleton();
+	if (!IsValid(AnimSkeleton))
+	{
+		UE::AssetValidation::AddTokenMessage(Context, EMessageSeverity::Error,
+			AssetData, LOCTEXT("NoSkeleton", "Animation asset is missing skeleton.")
+		);
+		return EDataValidationResult::Invalid;
+	}
+
+	if (AnimSkeleton != Skeleton)
+	{
+		UE::AssetValidation::AddTokenMessage(Context, EMessageSeverity::Error, AssetData, FString{" "}, FAssetData{&AnimAsset},
+			FText::Format(LOCTEXT("SkeletonMismatch", "Animation asset has a different skeleton. Expected [{0}], actual [{1}]"),
+				FText::FromString(Skeleton->GetName()), FText::FromString(AnimSkeleton->GetName()))
+		);
+		return EDataValidationResult::Invalid;
+	}
+
+	return EDataValidationResult::Valid;
+}
+
+EDataValidationResult UAssetValidator_AnimationAsset::ValidateCurves(const UAnimationAsset& AnimAsset, const FAssetData& AssetData, FDataValidationContext& Context)
+{
+	const USkeleton* Skeleton = AnimAsset.GetSkeleton();
+	if (!IsValid(Skeleton))
+	{
+		return EDataValidationResult::Valid;
+	}
+
+	const UAnimSequenceBase* AnimSequence = Cast<UAnimSequenceBase>(&AnimAsset);
+	if (AnimSequence == nullptr)
+	{
+		return EDataValidationResult::Valid;
+	}
+	
+	const FAssetData SkeletonAsset{Skeleton};
+	const TArray<FFloatCurve>& Curves = AnimSequence->GetCurveData().FloatCurves;
+
+	EDataValidationResult Result = EDataValidationResult::Valid;
+	for (const FFloatCurve& Curve: Curves)
+	{
+		const FName CurveName = Curve.GetName();
+		if (!CurveExists(*Skeleton, CurveName))
+		{
+			UE::AssetValidation::AddTokenMessage(Context, EMessageSeverity::Error, AssetData,
+				FText::Format(LOCTEXT("NoCurveFormat", "No curve [{0}] present on skeleton "), FText::FromName(CurveName)),
+				SkeletonAsset, LOCTEXT("CurveExistsOnAsset", ", but exists on animation sequence.")
+			);
+			Result &= EDataValidationResult::Invalid;
+		}
+	}
+
+	return Result;
+}
+
+bool UAssetValidator_AnimationAsset::CurveExists(const USkeleton& Skeleton, FName CurveName)
+{
+	return Skeleton.GetCurveMetaData(CurveName) != nullptr;
+}
+
+#undef LOCTEXT_NAMESPACE
--- Source/AssetValidation/Public/AssetValidators/AssetValidator_AnimationAsset.h
+++ Source/AssetValidation/Public/AssetValidators/AssetValidator_AnimationAsset.h
@@ -1,21 +1,21 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "AssetValidator.h"
+#include "AssetValidators/AssetValidator.h"
 
-#include "AssetValidator_AnimSequence.generated.h"
+#include "AssetValidator_AnimationAsset.generated.h"
 
 UCLASS()
-class ASSETVALIDATION_API UAssetValidator_AnimSequence: public UAssetValidator
+class ASSETVALIDATION_API UAssetValidator_AnimationAsset: public UAssetValidator
 {
 	GENERATED_BODY()
 public:
-	
 	//~Begin EditorValidatorBase interface
 	virtual bool CanValidateAsset_Implementation(const FAssetData& InAssetData, UObject* InObject, FDataValidationContext& InContext) const override;
 	virtual EDataValidationResult ValidateLoadedAsset_Implementation(const FAssetData& InAssetData, UObject* InAsset, FDataValidationContext& Context) override;
 	//~End EditorValidatorBase interface
 
-	static EDataValidationResult ValidateCurves(const USkeleton& Skeleton, const UAnimSequenceBase& AnimSequence, const FAssetData& AssetData, FDataValidationContext& Context);
+	static EDataValidationResult ValidateSkeleton(const USkeleton* Skeleton, const UAnimationAsset& AnimAsset, const FAssetData& AssetData, FDataValidationContext& Context);
+	static EDataValidationResult ValidateCurves(const UAnimationAsset& AnimAsset, const FAssetData& AssetData, FDataValidationContext& Context);
 	static bool CurveExists(const USkeleton& Skeleton, FName CurveName);
 };
