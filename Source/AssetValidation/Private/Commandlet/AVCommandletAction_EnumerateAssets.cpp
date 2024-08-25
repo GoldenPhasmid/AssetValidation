f9bd17be8043bef5dc75ff55471dfd0168e7fb12
phasmid <okushnirenko.work@gmail.com>
2024-08-25 10:45:46 +0300

    Enumerate assets commandlet action

---
+++ Source/AssetValidation/Private/Commandlet/AVCommandletAction_EnumerateAssets.cpp
@@ -0,0 +1,9 @@
+﻿#include "Commandlet/AVCommandletAction_EnumerateAssets.h"
+
+#include "AssetValidationDefines.h"
+
+bool UAVCommandletAction_EnumerateAssets::Run(const TArray<FAssetData>& Assets)
+{
+	UE_LOG(LogAssetValidation, Display, TEXT("UAssetValidationCommandlet: Found %d assets."), Assets.Num());
+	return true;
+}
---
+++ Source/AssetValidation/Public/Commandlet/AVCommandletAction_EnumerateAssets.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "AVCommandletAction.h"
+
+#include "AVCommandletAction_EnumerateAssets.generated.h"
+
+UCLASS(DisplayName = "Enumerate Assets")
+class UAVCommandletAction_EnumerateAssets: public UAVCommandletAction
+{
+	GENERATED_BODY()
+public:
+
+	virtual bool Run(const TArray<FAssetData>& Assets) override;
+};
