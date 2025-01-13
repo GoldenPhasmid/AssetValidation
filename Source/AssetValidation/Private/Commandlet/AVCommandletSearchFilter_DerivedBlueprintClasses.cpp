682b9768a0ab6a004943e4a7ca48fba29ff0427d
phasmid <okushnirenko.work@gmail.com>
2025-01-13 12:05:33 +0200

    Implement search filter for "Derived Blueprint Classes"

---
+++ Source/AssetValidation/Private/Commandlet/AVCommandletSearchFilter_DerivedBlueprintClasses.cpp
@@ -0,0 +1,83 @@
+﻿#include "Commandlet/AVCommandletSearchFilter_DerivedBlueprintClasses.h"
+
+#include "AssetValidationSettings.h"
+#include "Algo/RemoveIf.h"
+#include "AssetRegistry/AssetRegistryModule.h"
+#include "AssetRegistry/IAssetRegistry.h"
+
+namespace UE::AssetValidation
+{
+	static const FString BaseClass{TEXT("BaseClass")};
+}
+
+UAVCommandletSearchFilter_DerivedBlueprintClasses::UAVCommandletSearchFilter_DerivedBlueprintClasses()
+{
+}
+
+void UAVCommandletSearchFilter_DerivedBlueprintClasses::PostInitProperties()
+{
+	Super::PostInitProperties();
+	
+	if (!HasAnyFlags(RF_ClassDefaultObject))
+	{
+		Paths = UAssetValidationSettings::Get()->ValidatePaths;
+	}
+}
+
+void UAVCommandletSearchFilter_DerivedBlueprintClasses::InitFromCommandlet(const TArray<FString>& Switches, const TMap<FString, FString>& Params)
+{
+	Super::InitFromCommandlet(Switches, Params);
+}
+
+bool UAVCommandletSearchFilter_DerivedBlueprintClasses::GetAssets(TArray<FAssetData>& OutAssets) const
+{
+	IAssetRegistry& AssetRegistry = FModuleManager::LoadModuleChecked<FAssetRegistryModule>(TEXT("AssetRegistry")).Get();
+	AssetRegistry.SearchAllAssets(true);
+	
+	TArray<UClass*> Classes;
+	GetDerivedClasses(BaseClass, Classes);
+	Classes.Add(BaseClass);
+	
+	TArray<FTopLevelAssetPath> BaseClasses;
+	BaseClasses.Reserve(Classes.Num());
+
+	for (UClass* Class: Classes)
+	{
+		BaseClasses.Add(Class->GetClassPathName());
+	}
+	
+	TSet<FTopLevelAssetPath> DerivedClasses;
+	AssetRegistry.GetDerivedClassNames(BaseClasses, {}, DerivedClasses);
+	
+	for (auto It = DerivedClasses.CreateIterator(); It; ++It)
+	{
+		const FName PackageName = It->GetPackageName();
+		const FString& PackageNameStr = PackageName.ToString();
+		if (FPackageName::IsScriptPackage(PackageNameStr))
+		{
+			continue;
+		}
+
+		bool bPathFilterPassed = Paths.IsEmpty();
+		for (const FDirectoryPath& Path: Paths)
+		{
+			if (PackageNameStr.StartsWith(Path.Path))
+			{
+				bPathFilterPassed = true;
+				break;
+			}
+		}
+
+		if (!bPathFilterPassed)
+		{
+			continue;
+		}
+
+		AssetRegistry.GetAssetsByPackageName(PackageName, OutAssets, false);
+	}
+
+	// leave only unique assets
+	OutAssets = TSet<FAssetData>{OutAssets}.Array();
+
+	return true;
+}
---
+++ Source/AssetValidation/Public/Commandlet/AVCommandletSearchFilter_DerivedBlueprintClasses.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "AVCommandletAssetSearchFilter.h"
+
+#include "AVCommandletSearchFilter_DerivedBlueprintClasses.generated.h"
+
+UCLASS(meta = (DisplayName = "Derived Blueprint Classes"))
+class UAVCommandletSearchFilter_DerivedBlueprintClasses: public UAVCommandletSearchFilter
+{
+	GENERATED_BODY()
+public:
+	UAVCommandletSearchFilter_DerivedBlueprintClasses();
+	
+	//~Begin CommandletSearchFilter interface
+	virtual void PostInitProperties() override;
+	virtual void InitFromCommandlet(const TArray<FString>& Switches, const TMap<FString, FString>& Params) override;
+	virtual bool GetAssets(TArray<FAssetData>& OutAssets) const override;
+	//~End CommandletSearchFilter interface
+
+	UPROPERTY(EditAnywhere, Category = "Filter")
+	UClass* BaseClass = nullptr;
+
+	/** Specify a list of package paths to include into search request. Empty means ALL */
+	UPROPERTY(EditAnywhere, Category = "Filter", meta = (Validate, ContentDir))
+	TArray<FDirectoryPath> Paths;
+};
